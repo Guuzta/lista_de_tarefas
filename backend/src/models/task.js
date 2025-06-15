f9b1f51636a20a4cc4acf0d07062069219ad437c
Guuzta <Guuhsilva13@outlook.com>
2025-06-15 15:36:07 -0300

    criando esquema do banco de dados

---
+++ backend/src/models/task.js
@@ -0,0 +1,11 @@
+const mongoose = require(mongoose)
+
+const { Schema } = mongoose
+
+const taskSchema = new Schema({
+    task: String
+})
+
+const Model = mongoose.model('tasks', taskSchema)
+
+module.exports = Model
