d72aaec89880d77fd85e384229cd8dd16af9014e
Guuzta <Guuhsilva13@outlook.com>
2025-06-25 23:08:57 -0300

    adicionando funcao para editar tarefa

--- backend/src/public/js/tasks.js
+++ backend/src/public/js/tasks.js
@@ -1,5 +1,6 @@
 const newTaskButton = document.getElementById('newTask')
 const deleteTaskButtons = document.querySelectorAll('#deleteTask')
+const editingTaskButtons = document.querySelectorAll('#editingTask')
 
 const API_URL = 'http://localhost:3000/api'
 
@@ -79,12 +80,79 @@ deleteTaskButtons.forEach((button) => {
         })
             .then(response => response.json())
             .then(data => {
-               if(data.message === 'sucess') {
+                if (data.message === 'sucess') {
                     location.reload()
                 }
             }).catch(error => {
                 console.error(error)
             })
 
+    }
+})
+
+editingTaskButtons.forEach((button) => {
+    button.onclick = function () {
+        Swal.fire({
+            title: 'Atualizar tarefa',
+            html: `
+            <input type="text" id="description" class="swal2-input" placeholder="Atualize sua tarefa aqui">
+        
+        `,
+            showDenyButton: true,
+            confirmButtonText: 'Atualizar',
+            denyButtonText: 'Cancelar',
+            customClass: {
+                popup: 'popupBackground',
+                title: 'title',
+                confirmButton: 'confirmButton',
+            }
+        }).then((result) => {
+            if (result.isConfirmed) {
+
+                const description = document.getElementById('description').value
+                const taskId = document.getElementById('taskId').value
+
+                console.log(taskId)
+
+                fetch(`${API_URL}/tasks/${taskId}`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        description,
+                    })
+                })
+
+
+                Swal.fire({
+                    showConfirmButton: false,
+                    title: 'Tarefa atualizada com sucesso!',
+                    icon: 'success',
+                    timer: 1300,
+                    customClass: {
+                        popup: 'popupBackground',
+                        title: 'title',
+                        confirmButton: 'confirmButton',
+                    }
+                })
+
+                setTimeout(function () {
+                    location.reload()
+                }, 1400)
+            } else {
+                Swal.fire({
+                    showConfirmButton: false,
+                    title: 'Sua tarefa não foi atualizada!',
+                    icon: 'error',
+                    timer: 1300,
+                    customClass: {
+                        popup: 'popupBackground',
+                        title: 'title',
+                        confirmButton: 'confirmButton',
+                    }
+                })
+            }
+        })
     }
 })
