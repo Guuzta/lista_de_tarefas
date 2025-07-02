a1336f3a72a5101cd7e4792df5383f30f669cbe3
Guuzta <Guuhsilva13@outlook.com>
2025-07-02 20:10:02 -0300

    conectando ao mongodb atlas

--- backend/src/database/database.js
+++ backend/src/database/database.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+dotenv.config()
 
 async function connect () {
 
     try {
-        await mongoose.connect('mongodb://localhost:27017/todolist')
+        await mongoose.connect(process.env.MONGODB_URL)
         console.log('Conexão realizada com sucesso ao banco de dados!')
     } catch (error) {
         console.error('Não foi possível se conectar ao banco de dados!', error)
--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ app.use('/', webRoutes)
 
 dataBase.connect()
 
-const port = 3000 || process.env.PORT
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}...`)
 })
